5e03c5b3c2636f2e764c6b9d3e92b730900f3947
alancipher <alansame86@gmail.com>
2021-12-19 18:02:13 -0500

    add some html the base layout and some test with home.html

--- website/auth.py
+++ website/auth.py
@@ -5,3 +5,18 @@ from flask import  Blueprint
 #blueprint nous avons des routes ici 
 
 auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return "<p> Login </p>"
+
+
+@auth.route('/logout')
+def logout():
+    return "<p> Loout </p>"
+
+
+
+@auth.route('/sig-up')
+def sign_up():
+    return "<p> Sign Up </p>"
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import  Blueprint
+from flask import  Blueprint, render_template
 
 #blueprint nous avons des routes ici 
 
@@ -7,4 +7,4 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1> Test </h1>"
+    return render_template("home.html")
