7ef2c88e2c7dcbbe932fae7b7ad27c6943a2765f
alancipher <alansame86@gmail.com>
2021-12-19 21:49:24 -0500

    create db adding new user and log in him

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,30 @@
 #authentification purpose 
 
-from flask import  Blueprint,render_template, request, flash
-
+from flask import  Blueprint,render_template, request, flash, url_for, redirect
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 #blueprint nous avons des routes ici 
-
+from . import db 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods= ['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password') 
+
+        user =User.query.filter_by(email=email).first()
+        if user :
+            if check_password_hash(user.password, password):
+                flash('logged in successfully!', category='success')
+            else:
+                flash('not correct input, try it again!', category='error')
+
+        else:
+            flash('User does not exists!', category='error')
+          
+          
+
     return  render_template("login.html", text="Texting", user = "Tim")
 
 
@@ -27,7 +42,13 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
+        user =User.query.filter_by(email=email).first()
+        if user :
+            flash('the user already exist', category='error')
+
+
         if len(email)< 4 :
+            
             flash('email must be greater than 4 chararter ', category='error')
         elif len(firsname)< 2 :
             flash('firstname  must be greater than 2 chararter ', category='error')
@@ -39,5 +60,7 @@ def sign_up():
             flash('password must have more than 7 characters', category='error')
         else :
             #add user to the db
+            user = User(email = email, firstname =firsname, password= password1 )
             flash('account created with succes ', category='success')
+            return redirect(url_for('views.home'))
     return render_template("sign_up.html")
